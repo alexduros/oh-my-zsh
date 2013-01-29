9817e552e81ab8dcc4783c473f2ea424fa035015
alexduros <alexandre.duros@gmail.com>
2013-01-29 23:00:48 +0100

    add the useless plugin for making snow

---
+++ plugins/useless/useless.plugin.zsh
@@ -0,0 +1,5 @@
+make_snow(){
+    clear; while :; do echo $LINES $COLUMNS $(($RANDOM%$COLUMNS)); sleep 0.1; done | awk '{a[$3]=0; for(x in a) {o=a[x]; a[x]=a[x]+1; printf "\033[%s;%sH ",o,x; printf "\033[%s;%sH*\033[0;0H",a[x],x;}}'
+}
+
+alias "snow"="make_snow"
